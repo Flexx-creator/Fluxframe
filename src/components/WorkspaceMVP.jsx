c9e0134339688f2a4047e120f9b7e05d0840f643
Flexx-creator <team.sketchd@gmail.com>
2025-05-23 14:21:34 +0100

    Create WorkspaceMVP.jsx

---
+++ src/components/WorkspaceMVP.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkspaceMVP = () => {
+  return (
+    <div>
+      <h2>AI Workspace MVP coming soon...</h2>
+    </div>
+  );
+};
+
+export default WorkspaceMVP;
